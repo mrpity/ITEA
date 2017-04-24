4bf772a924ce0055b83f48bceebf9534d6d23a6a
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2017-04-24 21:46:28 +0300

    patterns - Observer, Command, Singletone

--- 24_04_2017/lesson.py
+++ 24_04_2017/lesson.py
@@ -132,3 +132,202 @@ class Multipler:
 double = Multipler(2)
 
 print(double(2))
+
+
+class scale:
+
+    def __init__(self, n):
+        self._n = n
+
+    def __call__(self, f):
+        def wrapper(x):
+            return f(x * self._n)
+        return wrapper
+
+@scale(5)
+def get_area(x):
+    return x * x
+
+print(get_area(5))
+
+##### РЕКУРСИВНАЯ функция
+
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n -1) + fib(n - 2)
+
+print(fib(20))
+
+
+class memo:
+
+    def __init__(self):
+        self._state = {}
+
+    def __call__(self, f):
+        def wrapper(n):
+            if n not in self._state:
+                self._state[n] = f(n)
+            return self._state[n]
+        return wrapper
+
+
+@memo()
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n -1) + fib(n -2)
+
+
+import profile
+# print(fib(10))
+# profile.run('fib(20)')
+
+m = memo()
+
+@m
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n -1) + fib(n -2)
+
+# profile.run('fib(10)')
+
+print(fib(10))
+print(m._state)
+
+
+print('--------------------------')
+
+class F:
+    def __new__(cls):
+        return 42
+
+g = F()
+
+print(g)
+print(type(g))
+
+print('-----------------------------------------------------')
+
+class Car:
+
+    def run(self):
+        print('I am car')
+
+class Truck:
+
+    def run(self):
+        print('I am truck')
+
+class Vehicle:
+
+    def __new__(cls, type_):
+        if type_ == 'Car':
+            return Car()
+        elif type_ == 'Truck':
+            return Truck()
+        else:
+            raise ValueError
+
+vehicles = ['Car', 'Car', 'Truck', 'Car']
+
+vehicles = [Vehicle(x) for x in vehicles]
+
+import pprint
+pprint.pprint(vehicles)
+
+for vehicles in vehicles:
+    vehicles.run()
+
+print('-------------------------------------------------------------')
+
+def f():
+    class A:
+        def f(self):
+            print('I am class A')
+    return A
+
+c = f()
+print(c)
+a = c()
+a.f()
+
+
+print('-------------------------------------------------------------')
+
+class Singleton():
+
+    def __new__(cls):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+
+s = Singleton()
+s1 = Singleton()
+
+print(s is s1)        ## Любой Синглтон будет одним и тем же экземпляром класса.
+
+class A:
+    db_conn = ...
+
+## Удаление атрибута
+del Singleton._instance
+
+
+####### PAttern Observer
+
+def a():
+    print('a')
+
+def b():
+    print('b')
+
+def c():
+    print('c')
+
+class Notifier:
+
+    def __init__(self):
+        self.observers = []
+
+    def register(self, observer):
+        self.observers.append(observer)
+
+    def unregister(self, observer):
+        self.observers.remove(observer)
+
+    def __call__(self, f):
+        def wrapper(*args, **kwargs):
+            res = f(*args, **kwargs)
+            for observer in self.observers:
+                observer()
+            return res
+        return wrapper
+
+# def notify(f):
+#     def wrapper(*args, **kwargs):
+#         res = f(*args, **kwargs)
+#         for observer in sobservers:
+#             observer()
+#         return res
+#     return wrapper
+
+# @notify
+notifier = Notifier()
+
+@notifier
+def f():
+    return 42
+
+print(f())
+
+notifier.register(a)
+print(f())
+
+# observers.append(b)
+# print(f())
+# observers.append(a)
+# print(f())
