d83d3311bfebd1ead265ccd4fbf6b06101969aaf
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2017-05-25 21:56:58 +0300

    different parsers

--- 25_05_2017/lesson.py
+++ 25_05_2017/lesson.py
@@ -41,3 +41,62 @@ if __name__ == '__main__':
     #print(tree.getroot())
     for el in tree.findall("day[@date='01.01.2018']//task"):
         print(el.text)
+
+
+########## DOM parser
+
+    from xml.dom import minidom
+
+    tree = minidom.parse('tasks.xml')
+    print(tree)
+    # pprint.pprint(dir(tree))
+    print(tree.firstChild.firstChild)
+
+    for el in tree.firstChild.firstChild.childNodes:
+        print(el.firstChild.wholeText)
+
+
+########## Stax parser
+
+    from xml.dom import pulldom
+
+    doc = pulldom.parse('tasks.xml')
+
+    for event, node in doc:
+        if event == pulldom.START_ELEMENT and node.localName == 'tasks':
+            doc.expandNode(node)
+            print(node.toxml())
+            print(node.firstChild.firstChild)
+
+
+######### SAX parser
+
+    from xml import sax
+
+    class TasksHandler(sax.ContentHandler):
+
+        def __init__(self):
+            super().__init__()
+            self.is_task = False
+
+        # открывающй тег
+        def startElement(self, name, attrs):
+            if name == 'task':
+                self.is_task = True
+
+        # закривающий тег
+        def endElement(self, name):
+            if name == 'task':
+                self.is_task = False
+
+        def characters(self, content):
+            if self.is_task:
+                print(content)
+
+
+    parser = sax.make_parser()
+    parser.setContentHandler(TasksHandler())
+
+    a = parser.parse(open('tasks.xml', 'rt'))
+
+    print(a)
