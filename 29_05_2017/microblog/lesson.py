766e50cb57e11597af2841aad8d94cc5a095d014
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2017-05-29 21:57:40 +0300

    Flask framework

---
+++ 29_05_2017/microblog/lesson.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)  ## __name__ что бы привязать фласк с текущей директории
+
+
+@app.route('/')
+#@app.route('/<name>')
+def index():
+    return render_template('index.html')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        print(request.form['post'], request.form['name'])
+
+        post =  request.form.get('post', '')
+        name = request.form.get('name', '')
+        if post and name:
+            print('!!!')
+    return render_template('add.html', name=name, post=post)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
