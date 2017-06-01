966c3a00d887dbbc22dee88d41f51f164630376e
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2017-06-01 20:05:22 +0300

    flask connect to db

--- 29_05_2017/microblog/lesson.py
+++ 29_05_2017/microblog/lesson.py
@@ -1,21 +1,42 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, g
+import sqlite3
 
 app = Flask(__name__)  ## __name__ что бы привязать фласк с текущей директории
+app.secret_key = 'asdadasda'
 
+
+@app.before_request
+def before_request():
+    g.db = sqlite3.connect('blog.db')
+
+@app.after_request
+def after_request(f):
+    g.db.close()
+    return f
 
 @app.route('/')
 #@app.route('/<name>')
 def index():
-    return render_template('index.html')
+    #db = sqlite3.connect('blog.db')
+    cursor = g.db.execute('select * from postss order by id desc')
+    postss = cursor.fetchall()
+    #db.close()
+    message = session.pop('message', '')
+    return render_template('index.html', postss=postss, message=message)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    name = post = ''
     if request.method == 'POST':
         print(request.form['post'], request.form['name'])
-
         post =  request.form.get('post', '')
         name = request.form.get('name', '')
         if post and name:
+            #db = sqlite3.connect('blog.db')
+            g.db.execute('INSERT INTO postss (name, post) VALUES (?,?)', (name,post))
+            g.db.commit()
+            #db.close()
+            session['message'] = 'Post has been added successfully'
             return redirect('/')
     return render_template('add.html', name=name, post=post)
 
