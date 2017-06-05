2ed319cada86fbecdca20d4f2991c82b9184834c
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2017-06-05 19:59:51 +0300

    different kind of profiles

---
+++ 05_06_2017/lesson.py
@@ -0,0 +1,30 @@
+import csv
+# import profile
+
+#@profile   #--- нужно профилировать тут функцию для которой хотим применинть line_profile. kernprog -l script.py
+def load():
+    res = []
+    with open('Pokemon.csv', 'rt') as f:
+        r = csv.reader(f)
+        next(r, None)  ## прочитать первую строку но никуда не записывать. Например пропустить заголовок.
+        for row in r:
+            res.append(row)
+    return res
+
+#@profile
+def max_total(r):
+    m = 0
+    for row in r:
+        if m < int(row[4]):
+            m = int(row[4])
+
+    return m
+
+if __name__ == '__main__':
+
+    # profile.run('pok_parse.max_total(pok_parse.load())')
+
+    # import profile
+    #profile.run('max_total(load())')
+
+    print(max_total(load()))
