ffd43c3ec136cabf07182771aa12a9bad4f8648d
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2017-10-27 17:34:51 +0300

    start to write my first flask app

---
+++ FLASK_APP/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'mr.pity!'
+
+@app.route('/user')
+def user(): pass
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show user profile
+    return 'User: %s' % username
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5001)
