ce32a7d75a1f3aa6bad46edcf363fd22af685e8f
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2017-12-21 00:40:26 +0200

    bitbucket oauth

---
+++ bitbucket-auth/bitbucket-auth.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import BackendApplicationClient
+
+
+class ClientSecrets:
+    '''
+    The structure of this class follows Google convention for `client_secrets.json`:
+    https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+    Bitbucket does not emit this structure so it must be manually constructed.
+    '''
+    client_id = "9FVKRtLkHUgMQpb8zU"
+    client_secret = "RQ3qH3LyvPBtGZPJpyZBfNLNmTCwAjLu"
+    redirect_uris = [
+      "https://localhost"  # Used for testing.
+    ]
+    auth_uri = "https://bitbucket.org/site/oauth2/authorize"
+    token_uri = "https://bitbucket.org/site/oauth2/access_token"
+    server_base_uri = "https://api.bitbucket.org/"
+
+
+def main():
+    c = ClientSecrets()
+    # Fetch a request token
+    bitbucket = OAuth2Session(c.client_id)
+    # Redirect user to Bitbucket for authorization
+    authorization_url = bitbucket.authorization_url(c.auth_uri)
+    print("Please go here and authorize: {}".format(authorization_url[0]))
+    # Get the authorization verifier code from the callback url
+    redirect_response = input("Paste the full redirect URL here:")
+    # Fetch the access token
+    bitbucket.fetch_token(
+      c.token_uri,
+      authorization_response=redirect_response,
+      username=c.client_id,
+      password=c.client_secret)
+    # Fetch a protected resource, i.e. user profile
+    r = bitbucket.get(c.server_base_uri + "1.0/user")
+    print(r.content)
+
+def main1():
+    client_id = "9FVKRtLkHUgMQpb8zU"
+    client_secret = "RQ3qH3LyvPBtGZPJpyZBfNLNmTCwAjLu"
+    server_base_uri = "https://api.bitbucket.org/"
+
+    client = BackendApplicationClient(client_id=client_id)
+    bitbucket = OAuth2Session(client=client)
+    token = bitbucket.fetch_token(token_url='https://bitbucket.org/site/oauth2/access_token', client_id=client_id,
+        client_secret=client_secret)
+    print(token)
+    r = bitbucket.get(server_base_uri + "1.0/user")
+    print(r.content)
+
+if __name__ == "__main__":
+#    main()
+     main1()
