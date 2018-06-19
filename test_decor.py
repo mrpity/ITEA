5a07675fe382b8d30b188c656ebc7b5c062cabdf
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2018-06-19 18:57:10 +0300

    test default value for connectors

--- test_decor.py
+++ test_decor.py
@@ -1,13 +1,30 @@
 
-def decor(f):
-    def wrapper():
-        print('----')
-        f()
-        print('---')
-    return wrapper
+# def decor(f):
+#     def wrapper():
+#         print('----')
+#         f()
+#         print('---')
+#     return wrapper
+#
+# #@decor
+# def f():
+#     print("test")
+#
+# z = decor(f)()
 
-@decor
-def f():
-    print("test")
 
-f()
+
+print("123")
+
+def test():
+    for x in range(0, 10):
+        print(x)
+        line = yield
+        print(line)
+        continue
+
+gen = test()
+gen.__next__()
+gen.send('sda')
+
+
