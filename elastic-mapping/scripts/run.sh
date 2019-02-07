e89cced3852323275e5c4ada076efa7d1c5477bf
Dima Khodakivskyi <d.khodakivsky@whirl.sg>
2019-02-07 18:41:12 +0200

    refactor

--- elastic-mapping/scripts/run.sh
+++ elastic-mapping/scripts/run.sh
@@ -3,5 +3,8 @@
 set -euo pipefail
 
 pip install --upgrade pip
-pip install -U --no-cache-dir -r requirements.txt
+pip install -U --no-cache-dir -r /workdir/requirements.txt
 
+echo "RUN: python3 elastic_utils.py -a create -m webpages posts -elastic $ELASTICSEARCH_URL"
+sleep 3600
+#exec python3 /workdir/elastic_utils.py -a create -m webpages posts -elastic $ELASTICSEARCH_URL
